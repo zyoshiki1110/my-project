e22cd4424338937c1f68b5f6e4b4fdda1accce91
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-26 15:59:11 +0900

    Create exX-XX.js

---
+++ exX-XX.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('action-btn');
+const img = document.getElementById('scary-image');
+const title = document.getElementById('main-title');
+const statusMsg = document.getElementById('status-msg');
+
+btn.addEventListener('click', function() {document.body.classList.add('inverted');btn.style.display = 'none';statusMsg.innerText = "ファイルをダウンロード中...";
+setTimeout(function() {img.src = "https://via.placeholder.com/800x600/ff0000/ffffff?text=ESCAPE+IMPOSSIBLE";
+        
+    
+        img.classList.add('scary-size');
+        
+        
+        title.innerText = "やばいかも";
+        statusMsg.innerText = "あなたは「それ」を見てしまいましたね。";
+
+        
+        alert("警告：システムが破壊されました");
+        
+    }, 2000); 
+});
