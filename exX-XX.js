61663b545a1c1c79b1546619ac2134b94a8aa241
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-26 17:06:30 +0900

    Update image and title handling on button click
    Refactor event listener to include image and title updates.

--- exX-XX.js
+++ exX-XX.js
@@ -1,5 +1,24 @@
-setTimeout(function() {img.src = "image/xxx.png";img.classList.add('scary-size');title.innerText = "やばいかも";
-   
-    alert("警告：システムが破壊されました");
+const btn = document.getElementById('action-btn');
+const img = document.getElementById('scary-image');
+const title = document.getElementById('main-title');
+const statusMsg = document.getElementById('status-msg');
 
-}, 2000);
+btn.addEventListener('click', function() {
+   
+    document.body.classList.add('inverted');
+    btn.style.display = 'none';
+    statusMsg.innerText = "ファイルをダウンロード中...";
+
+  
+    setTimeout(function() {
+       
+        img.src = "image/xxx.png";
+        img.classList.add('scary-size');
+        
+        title.innerText = "やばいかも";
+        statusMsg.innerText = "あなたは「それ」を見てしまいましたね。";
+
+        alert("警告：システムが破壊されました");
+
+    }, 2000);
+});
