400a435570163d4d08b32ffe3c40920a460dfb01
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-30 19:19:49 +0900

    Update exX-XX.js

--- exX-XX.js
+++ exX-XX.js
@@ -1,38 +1,46 @@
 function startDokyo() {
-    
-    document.getElementById('action-btn').style.display = 'none';
-    document.getElementById('status-msg').innerText = "ファイルをダウンロード中...";
-    
-    countDownFnc(10);
+  document.getElementById('action-btn').style.display = 'none';
+  document.getElementById('status-msg').innerText = "ファイルダウンロード中...";
+  countDownFnc(10);
 }
 
 function countDownFnc(seconds) {
-    var target = document.getElementById('timer-display');
-    
-    if (seconds > 0) {
-        target.innerText = seconds;
-       
-        var nextSeconds = seconds - 1;
-        
-        setTimeout(function() {
-            countDownFnc(nextSeconds);
-        }, 1000);
-    } else {
-        target.innerText = "";
-        showDokkiri();
-    }
+  var target = document.getElementById('timer-display');
+  if (seconds > 0) {
+    target.innerText = seconds;
+    var next = seconds - 1;
+    setTimeout(function() {
+      countDownFnc(next);
+    }, 1000);
+  } else {
+    target.innerText = "";
+   
+    checkUser();
+  }
 }
 
-function showDokkiri() {
-    var scaryImg = document.getElementById('scary-image');
-    
-    
-    document.body.className = 'inverted';
-    scaryImg.src = "image/xxx.png";
-    scaryImg.className = 'scary-size';
-    
-    document.getElementById('main-title').innerText = "システムエラー";
-    document.getElementById('status-msg').innerText = "不正なアクセスを検知しました。";
+function checkUser() {
+  var result = confirm("ファイルをダウンロードします");
 
-　setTimeout(function(){alert("⚠️警告⚠️：システムが破壊されました");}, 50);
+  if (result == true) {
+    executeFinal();
+  } else {
+    alert("ダウンロードを開始しますか？。");
+    checkUser();
+  }
+}
+
+function executeFinal() {
+  var scaryImg = document.getElementById('scary-image');
+
+  document.body.className = 'inverted';
+  scaryImg.src = "image/xxx.png";
+  scaryImg.className = 'scary-size';
+  
+  document.getElementById('main-title').innerText = "システムエラー";
+  document.getElementById('status-msg').innerText = "不正なアクセスを検知しました。";
+
+  setTimeout(function(){
+    alert("⚠️警告⚠️：システムが破壊されました");
+  }, 50);
 }
