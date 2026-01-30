609d934877305bc227569999acd74aedf89f7cf4
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-30 19:15:50 +0900

    Update exX-XX.js

--- exX-XX.js
+++ exX-XX.js
@@ -34,5 +34,5 @@ function showDokkiri() {
     document.getElementById('main-title').innerText = "システムエラー";
     document.getElementById('status-msg').innerText = "不正なアクセスを検知しました。";
 
-    alert("⚠️警告⚠️：システムが破壊されました");
+　setTimeout(function(){alert("⚠️警告⚠️：システムが破壊されました");}, 50);
 }
